314bcd70d60f3ff19fe38a132ae465d882d43ed6
ReggeMeister <mattiaswaern04@gmail.com>
2026-01-16 00:09:15 +0100

    gav upp... börjar om istället LOCK IN!!!!!!!

---
+++ milkshake/src/MilkshakeReviews.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import { Plus, X, Trophy, Swords, Target, Download } from 'lucide-react';
+import ReviewCard from './components/ReviewCard';
+import './styles/App.css';
+
+export default function MilkshakeReviews (){
+  
+}
